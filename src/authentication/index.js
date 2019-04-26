f39af71e8ea8b1e4370f3473548a046af14eee6d
Gerik Bonaert <gbo@escaux.com>
2019-04-26 13:28:40 +0200

    Provide authentication service

---
+++ src/authentication/index.js
@@ -0,0 +1,77 @@
+'use strict'
+
+const Bell = require('bell')
+const Joi = require('joi')
+const Jwt = require('jsonwebtoken')
+
+
+exports.plugin = {
+  pkg: require('./package.json'),
+  dependencies: [
+    'bell',
+    'hapi-auth-cookie'
+  ],
+  register: async (server, options) => {
+    const config = Joi.attempt(options, Joi.object().required().keys({
+      oauth: Joi.object({
+        uri: Joi.string().optional(),
+        scopes: Joi.array().default([]),
+        clientId: Joi.string(),
+        secret: Joi.string(),
+        cookie: Joi.object({ 
+          password: Joi.string().min(32)
+        })
+      }),
+      cookie: Joi.object().required().keys({
+        name: Joi.string().optional().default('sid'),
+        password: Joi.string().min(32)
+      })
+    }))
+
+    await server.register(Bell, { once: true })
+
+    server.auth.strategy('session', 'cookie', {
+      cookie: {
+        name: config.cookie.name,
+        password: config.cookie.password,
+        isSecure: false
+      },
+      redirectTo: '/login',
+      validateFunc: async (request, session) => {
+        const now = (new Date()).getTime() / 1000
+        return { valid: session.payload.iat < now && now < session.payload.exp }
+      }
+    })
+
+    server.auth.strategy('oauth', 'bell', {
+      provider: require('./providers/bnpparibasfortis')(config.oauth),
+      password: config.oauth.cookie.password,
+      clientId: config.oauth.clientId,
+      clientSecret: config.oauth.secret,
+      forceHttps: true,
+      isSecure: false
+    })
+
+    server.route({
+      method: 'GET',
+      path: '/login',
+      options: {
+        auth: 'oauth',
+        handler: (request, h) => {
+          const { token } = request.auth.credentials
+          const payload = Jwt.decode(token)
+
+          const profile = {
+            token,
+            payload
+          }
+
+          request.cookieAuth.clear()
+          request.cookieAuth.set(profile)
+
+          return h.redirect('/')
+        }
+      }
+    })
+  }
+}
---
+++ src/authentication/providers/bnpparibasfortis.js
@@ -0,0 +1,18 @@
+module.exports = function (options) {
+  options = options || {}
+
+  const uri = options.uri || 'https://sandbox.auth.bnpparibasfortis.com/'
+  const scopes = options.scopes
+
+  return {
+    protocol: 'oauth2',
+    useParamsAuth: true,
+    auth: uri + '/authorize',
+    token: uri + '/token',
+    scope: scopes,
+    scopeSeparator: '\n',
+    headers: {
+      'User-Agent': 'HAPI Escaux'
+    }
+  }
+}
